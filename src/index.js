f8ceb4554bf619f58b0df81d30720d3ea15ce2d7
AfroConnect <mensahmedia23@gmail.com>
2023-05-07 08:34:56 -0400

    complete project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("#create-task-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buildToDoList(e.target.elements["new-task-description"].value);
+    form.reset();
+  });
+
+  function buildToDoList(todo) {
+    let list = document.createElement("li");
+    let btn = document.createElement("button");
+    btn.addEventListener("click", handleDelete);
+    btn.textContent = "x";
+    list.textContent = todo;
+    list.appendChild(btn);
+    console.log(list);
+    document.querySelector("#tasks").appendChild(list);
+  }
+
+  function handleDelete(e) {
+    e.target.parentNode.remove();
+  }
 });
+
+
+
+
+
+
+
+
+
